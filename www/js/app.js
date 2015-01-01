01e0eca409f627b90065e968c9b859c86b0a2460
Steven Occhipinti <steve.occhipinti@rea-group.com>
2015-01-01 20:42:25 +1100

    Basic implementation of using local storage
    This controller is pretty chunky now and should be refactored!

--- www/js/app.js
+++ www/js/app.js
@@ -1,17 +1,37 @@
 angular.module('shoppinglist', ['ionic'])
 
-.factory('Item', ['$window', function($window) {
-  return {
-    get: function() {
-      return JSON.parse($window.localStorage['items'] || '[]');
-    },
-    set: function(value) {
-      $window.localStorage['items'] = JSON.stringify(value);
-    }
-  }
-}])
+.controller('ShoppingListCtrl', function($scope, $window) {
+  // Get the items from localstorage and save them back when they change
+  $scope.items = JSON.parse($window.localStorage['items'] || '[]');
+  $scope.$watch('items', function(updatedItems) {
+    $window.localStorage['items'] = JSON.stringify(updatedItems);
+  }, true);
 
-.controller('ShoppingListCtrl', function($scope, Item) {
+  // Add a new item to the list
+  $scope.addItem = function() {
+    if ($scope.items.map(function(x) { return x.title }).indexOf($scope.newItem) == -1) {
+      $scope.items.push(
+        { title: $scope.newItem, checked: false }
+      );
+    }
+    $scope.newItem = "";
+  };
+
+  // Clear all those ticked off items
+  $scope.clearDone = function() {
+    $scope.items = $scope.items.filter(function(item) {
+      return !item.checked
+    });
+  };
+
+  // Make those fancy delete buttons
+  $scope.onItemDelete = function(item) {
+    $scope.items.splice($scope.items.indexOf(item), 1);
+  };
+  $scope.data = {
+    showDelete: false
+  };
+
   // Prevent the soft keyboard from disappearing when clicking the "+" button
   // ref: https://stackoverflow.com/questions/7621711/how-to-prevent-blur-running-when-clicking-a-link-in-jquery
   var clickedAddButton = false;
@@ -24,27 +44,4 @@ angular.module('shoppinglist', ['ionic'])
   $('#addItem').click(function() {
     clickedAddButton = true;
   });
-
-  $scope.items = [];
-
-  $scope.addItem = function() {
-    $scope.items.push(
-      { title: $scope.newItem, checked: false }
-    );
-    $scope.newItem = "";
-  };
-
-  $scope.clearDone = function() {
-    $scope.items = $scope.items.filter(function(item) {
-      return !item.checked
-    });
-  };
-
-
-  $scope.onItemDelete = function(item) {
-    $scope.items.splice($scope.items.indexOf(item), 1);
-  };
-  $scope.data = {
-    showDelete: false
-  };
 })
