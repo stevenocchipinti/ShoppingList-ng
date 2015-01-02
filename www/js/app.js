e52af579d1a008bac4b1b9350b21cce5c211bdf7
Steven Occhipinti <steve.occhipinti@rea-group.com>
2015-01-02 16:53:28 +1100

    WIP - Autocomplete functionality working!
    Just with test data

--- www/js/app.js
+++ www/js/app.js
@@ -30,24 +30,56 @@ angular.module('shoppinglist', ['ionic'])
 
 .controller('ShoppingListCtrl', function($scope, ShoppingList) {
   $scope.shoppingList = ShoppingList;
+  var newItemElm = $('#newItem');
 
-  $scope.addItem = function() {
-    ShoppingList.add($scope.newItem);
+  $scope.addItem = function(item) {
+    ShoppingList.add(item);
     $scope.newItem = "";
   };
 
   // Prevent the soft keyboard from disappearing when clicking the "+" button
   // ref: https://stackoverflow.com/questions/7621711/how-to-prevent-blur-running-when-clicking-a-link-in-jquery
   var clickedAddButton = false;
-  $('#newItem').blur(function() {
+  newItemElm.blur(function() {
+    newItemElm.val("");
     if (clickedAddButton) {
-      $('#newItem').focus();
+      newItemElm.focus();
       clickedAddButton = false;
     }
   });
   $('#addItem').click(function() {
     clickedAddButton = true;
   });
+  $('.tt-dropdown-menu').click(function() {
+    clickedAddButton = true;
+  });
+
+
+  $scope.$watch('newItem', function(text) {
+    var suggestions = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+      'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+      'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+      'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+      'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+      'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+      'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+      'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+      'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+    ];
+    $scope.suggestions = [];
+
+    if (text && !text.isEmpty()) {
+      var matches = [];
+      var substrRegex = new RegExp(text, 'i');
+      $.each(suggestions, function(i, str) {
+        if (substrRegex.test(str)) {
+          matches.push(str);
+        }
+      });
+      $scope.suggestions = matches;
+    }
+  });
+
 })
 
 
