7e4da2ef6d2730a13d5871cbc8437820f8b320d2
Steven Occhipinti <steve.occhipinti@rea-group.com>
2015-01-02 21:01:56 +1100

    Extract an un-finished autocomplete directive
    At the moment, this is acting more like a partial than a directive.
    It still relies on the scope of the controller, but after I learn more
    I'll make it more isolated!

--- www/js/app.js
+++ www/js/app.js
@@ -59,6 +59,14 @@ angular.module('shoppinglist', ['ionic'])
 })
 
 
+.directive('autocomplete', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'autocomplete.html'
+  };
+})
+
+
 .controller('ShoppingListCtrl', function($scope, ShoppingList, Catalogue) {
   $scope.shoppingList = ShoppingList;
   var newItemElm = $('#newItem');
